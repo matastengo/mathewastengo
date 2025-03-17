19416bacaad21566edecc850901174cf9cc7b447
matastengo <31029999+matastengo@users.noreply.github.com>
2025-03-17 11:55:12 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,8 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+ // Activate SimpleLightbox plugin for portfolio items
+ new SimpleLightbox({
+    elements: '#portfolio a.portfolio-box'
+});
